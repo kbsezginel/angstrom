6e2172a2cbcf6519ca2cfc0dd8733bce68015d84
kbsezginel <kbs37@pitt.edu>
2018-01-30 11:17:57 -0500

    Add xyz file format read method.

---
+++ angstrom/molecule/read.py
@@ -0,0 +1,24 @@
+"""
+Method for reading chemical file formats.
+"""
+import numpy as np
+
+
+def read_xyz(filename):
+    """Read xyz file format
+
+    Args:
+        - filename (str): xyz file name
+
+    Returns:
+        - dict: atom names and coordinates
+            -> {'atoms': ['C', ...], 'coordinates': [[x1, y1, z1], ...]}
+    """
+    with open(filename, 'r') as xyz_file:
+        xyz_lines = xyz_file.readlines()
+    n_atoms = int(xyz_lines[0].strip())
+    atoms, coordinates = np.empty((n_atoms,), dtype=str), np.empty((n_atoms, 3))
+    for i, line in enumerate(xyz_lines[2:]):
+        atoms[i] = line.split()[0]
+        coordinates[i] = [float(i) for i in line.split()[1:4]]
+    return dict(atoms=atoms, coordinates=coordinates)
