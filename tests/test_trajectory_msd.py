ebcbac5c7df4b43fccc98b3d46496f99974d673e
kbsezginel <kbs37@pitt.edu>
2018-02-04 18:20:03 -0500

    Add MSD test for benzene moving in x.

---
+++ tests/test_trajectory_msd.py
@@ -0,0 +1,14 @@
+"""
+--- Ångström ---
+Tests calculating mean squared displacement for a trajectory.
+"""
+from angstrom import Trajectory
+import os
+
+benzene_traj_x = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'benzene-traj-x.xyz')
+
+
+def test_trajectory_msd_of_benzene_moving_linearly_in_x():
+    """Tests MSD calculation for benzene moving linearly in +x direction"""
+    benzene = Trajectory(read=benzene_traj_x)
+    assert benzene.get_msd(benzene.coordinates[:, 0, 0]) == 808.5
