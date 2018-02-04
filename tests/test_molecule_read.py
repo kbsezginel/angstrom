d504490396b0eaef390a0378a9ead94a0586eada
kbsezginel <kbs37@pitt.edu>
2018-02-04 16:57:54 -0500

    Rename benzene xyz read test.

--- tests/test_molecule_read.py
+++ tests/test_molecule_read.py
@@ -17,7 +17,7 @@ benzene_coors = [[0.00000, 1.40272, 0.00000], [0.00000, 2.49029, 0.00000],
                  [1.21479, 0.70136, 0.00000], [2.15666, 1.24515, 0.00000]]
 
 
-def test_read_xyz_molecule():
+def test_read_xyz_benzene_molecule():
     """Tests reading xyz formatted molecule file"""
     benzene = Molecule(read=benzene_xyz)
     assert len(benzene.atoms) == 12
