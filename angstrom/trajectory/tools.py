e0c7c7c567e511e2a1b57e4656741521521e9523
kbsezginel <kbs37@pitt.edu>
2018-02-05 11:12:04 -0500

    Fix non-periodic coordinates typos.

--- angstrom/trajectory/tools.py
+++ angstrom/trajectory/tools.py
@@ -16,20 +16,20 @@ def non_periodic_coordinates(coordinates, simulation_box):
     Returns:
         - ndarray: 3D list of non-periodic coordinates
     """
-    n_frames, n_atoms, _ = np.shape(traj.coordinates)
+    n_frames, n_atoms, _ = np.shape(coordinates)
     nonp_coordinates = np.empty((n_frames, n_atoms, 3))
     nonp_coordinates[0] = coordinates[0].copy()
     for a in range(n_atoms):
         modifier = np.zeros(3)
         for f in range(1, n_frames):
-            coor = traj.coordinates[f][a].copy()
-            prev_coor = traj.coordinates[f - 1][a]
+            coor = coordinates[f][a].copy()
+            prev_coor = coordinates[f - 1][a]
             for i in range(3):
                 dist = coor[i] - prev_coor[i]
-                if dist > sim_box[i] * 0.5:
-                    modifier[i] -= sim_box[i]
-                elif dist <= -sim_box[i] * 0.5:
-                    modifier[i] += sim_box[i]
+                if dist > simulation_box[i] * 0.5:
+                    modifier[i] -= simulation_box[i]
+                elif dist <= -simulation_box[i] * 0.5:
+                    modifier[i] += simulation_box[i]
                 coor[i] += modifier[i]
             nonp_coordinates[f][a] = coor
     return nonp_coordinates
