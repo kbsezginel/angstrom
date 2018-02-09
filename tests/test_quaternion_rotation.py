cf475a7ea01b8a8470ee7614655848bd7bfa1954
kbsezginel <kbs37@pitt.edu>
2018-02-09 16:58:02 -0500

    Add quaternion 90 degree rotation tests.

---
+++ tests/test_quaternion_rotation.py
@@ -0,0 +1,20 @@
+"""
+--- Ångström ---
+Tests Quaternion rotations.
+"""
+from angstrom.geometry.quaternion import Quaternion
+import numpy as np
+
+
+def test_single_point_90_degrees_rotation():
+    """ """
+    Q = Quaternion([0, 1, 1, 1])
+    # Rotate (1, 0, 0) around z-axis first counter-clockwise then clockwise
+    assert np.allclose(Q.rotation([1, 0, 0], [0, 0, 0], [0, 0, 1], np.pi / 2).xyz(), [0, 1, 0])
+    assert np.allclose(Q.rotation([1, 0, 0], [0, 0, 0], [0, 0, 1], -np.pi / 2).xyz(), [0, -1, 0])
+    # Rotate (0, 1, 0) around x-axis first counter-clockwise then clockwise
+    assert np.allclose(Q.rotation([0, 1, 0], [0, 0, 0], [1, 0, 0], np.pi / 2).xyz(), [0, 0, 1])
+    assert np.allclose(Q.rotation([0, 1, 0], [0, 0, 0], [1, 0, 0], -np.pi / 2).xyz(), [0, 0, -1])
+    # Rotate (0, 0, 1) around y-axis first counter-clockwise then clockwise
+    assert np.allclose(Q.rotation([0, 0, 1], [0, 0, 0], [0, 1, 0], np.pi / 2).xyz(), [1, 0, 0])
+    assert np.allclose(Q.rotation([0, 0, 1], [0, 0, 0], [0, 1, 0], -np.pi / 2).xyz(), [-1, 0, 0])
