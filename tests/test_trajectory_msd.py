d2eab987257907636ff8985b15dd398e1301384e
kbsezginel <kbs37@pitt.edu>
2018-02-12 16:19:05 -0500

    Add y and z direction msd assertion to benzene traj msd test.

--- tests/test_trajectory_msd.py
+++ tests/test_trajectory_msd.py
@@ -12,3 +12,5 @@ def test_trajectory_msd_of_benzene_moving_linearly_in_x():
     """Tests MSD calculation for benzene moving linearly in +x direction"""
     benzene = Trajectory(read=benzene_traj_x)
     assert benzene.get_msd(benzene.coordinates[:, 0, 0]) == 808.5
+    assert benzene.get_msd(benzene.coordinates[:, 0, 1]) == 0
+    assert benzene.get_msd(benzene.coordinates[:, 0, 2]) == 0
