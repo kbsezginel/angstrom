850f3269293ba4600c2af723aa18b6a470b5da0f
kbsezginel <kbs37@pitt.edu>
2018-03-21 01:55:30 -0400

    OpenBabel render function added.

--- angstrom/visualize/render.py
+++ angstrom/visualize/render.py
@@ -2,7 +2,20 @@
 --- Ångström ---
 Render molecular images and animations.
 """
+import subprocess
 
 
 def render():
     pass
+
+
+def render_openbabel(mol_file, img_file, arguments=['-xS', '-xd', 'xb', 'none']):
+    """ Render molecular images using OpenBabel
+
+    Args:
+        - mol_file (str): Molecule file
+        - img_file (str): Image file
+        - arguments (list): List of command line arguments
+    """
+    command = ['obabel', '%s' % mol_file, '-O', '%s' % img_file] + arguments
+    subprocess.call(command)
