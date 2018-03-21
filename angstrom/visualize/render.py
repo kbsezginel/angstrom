c433024def78b5b9f943fc8b2956beaa3816ca21
kbsezginel <kbs37@pitt.edu>
2018-03-21 01:59:06 -0400

    Blender render function placeholder.

--- angstrom/visualize/render.py
+++ angstrom/visualize/render.py
@@ -19,3 +19,18 @@ def render_openbabel(mol_file, img_file, arguments=['-xS', '-xd', 'xb', 'none'])
     """
     command = ['obabel', '%s' % mol_file, '-O', '%s' % img_file] + arguments
     subprocess.call(command)
+
+
+def render_blender(mol_file, save=None):
+    """ Render molecular images using Blender
+
+    Args:
+        - mol_file (str): Molecule file in pdb format
+        - save (str or None): Save blender file
+    """
+    # command = ['obabel', '%s' % mol_file, '-O', '%s' % img_file] + arguments
+    if save is not None:
+        # Save blender file
+    else:
+        # Render
+    pass
