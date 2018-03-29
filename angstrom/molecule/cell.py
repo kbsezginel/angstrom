32e2b3d274d7d33a1c5d407199be52d4eb923e54
kbsezginel <kbs37@pitt.edu>
2018-03-29 11:28:47 -0400

    Cell vectors calculation.

--- angstrom/molecule/cell.py
+++ angstrom/molecule/cell.py
@@ -13,10 +13,18 @@ class Cell:
         self.alpha, self.beta, self.gamma = [np.radians(i) for i in cellpar[3:]]
 
     def calculate_volume(self):
-        """
-        Calculates cell volume.
-        """
+        """ Calculates cell volume. """
         volume = 1 - np.cos(self.alpha)**2 - np.cos(self.beta)**2 - np.cos(self.gamma)**2
         volume += 2 * np.cos(self.alpha) * np.cos(self.beta) * np.cos(self.gamma)
         self.volume = self.a * self.b * self.c * np.sqrt(volume)
         self.frac_volume = volume / (self.a * self.b * self.c)
+
+    def calculate_vectors(self):
+        """ Calculates cell vectors. """
+        x_v = [self.a, 0, 0]
+        y_v = [self.b * np.cos(self.gamma), self.b * np.sin(self.gamma), 0]
+        z_v = [0.0] * 3
+        z_v[0] = self.c * np.cos(self.beta)
+        z_v[1] = (self.c * self.b * np.cos(self.alpha) - y_v[0] * z_v[0]) / y_v[1]
+        z_v[2] = np.sqrt(self.c * self.c - z_v[0] * z_v[0] - z_v[1] * z_v[1])
+        self.vectors = [x_v, y_v, z_v]
