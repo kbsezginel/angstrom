a95ac016b0029529441deb8ed2a831d016b60f92
kbsezginel <kbs37@pitt.edu>
2018-04-23 17:59:19 -0400

    Update show function to use single funciton for arranging molecule positions.

--- angstrom/visualize/show.py
+++ angstrom/visualize/show.py
@@ -3,6 +3,9 @@
 Show molecular images and animations.
 Works well with Jupyter notebook to visualize molecules in browser.
 """
+from .tools import arrange_molecules
+from angstrom.molecule.write import write_pdb
+import numpy as np
 import tempfile
 import nglview
 
@@ -16,36 +19,16 @@ def show(*args, camera='perspective', move='auto', div=5, distance=(-10, -10), a
         - axis: separation direction
         - caps: capitalize atom names so they show true colors in nglview
     """
-    if move is 'auto':
-        translation_vectors = arrange_structure_positions(len(args), div=div, distance=distance)
-    elif move is 'single':
-        translation_vectors = axis_translation(len(args), distance=distance[0], axis=axis)
-    else:
-        translation_vectors = [[0, 0, 0]] * len(args)
-
-    atom_names = []
-    atom_coors = []
-    group_numbers = []
-    for mol_index, (molecule, vec) in enumerate(zip(args, translation_vectors), start=1):
-        atom_names += molecule.atom_names
-        atom_coors += translate(molecule.atom_coors, vector=vec)
-        group_numbers += [mol_index] * len(molecule.atom_names)
-
-    # nglview require atom names in all caps to color them properly
-    if caps:
-        atom_names = [name.upper() for name in atom_names]
-
-    if not group:
-        group_numbers = [1] * len(atom_names)
+    atoms, coordinates, group_numbers = arrange_molecules(args, move=move, div=div, distance=distance, axis=axis, caps=caps, group=group)
 
     if save is None:
         temp_pdb_file = tempfile.NamedTemporaryFile(mode='w+', suffix='.pdb')
-        write_pdb(temp_pdb_file, atom_names, atom_coors, group=group_numbers)
+        write_pdb(temp_pdb_file, atoms, coordinates, group=group_numbers)
         view = nglview.show_structure_file(temp_pdb_file.name)
         temp_pdb_file.close()
     else:
         with open(save, 'w') as save_file:
-            write_pdb(save_file, atom_names, atom_coors, group=group_numbers)
+            write_pdb(save_file, atoms, coordinates, group=group_numbers)
         view = nglview.show_structure_file(save)
 
     view.camera = camera
