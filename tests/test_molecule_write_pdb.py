9408fca15373474b1a53c863ee8ae41b3c6a825b
kbsezginel <kbs37@pitt.edu>
2018-04-24 17:49:27 -0400

    Add pdb write tests, rename xyz write test.

---
+++ tests/test_molecule_write_pdb.py
@@ -0,0 +1,36 @@
+"""
+--- Ångström ---
+Tests writing molecule object.
+"""
+from angstrom import Molecule
+import filecmp
+import os
+
+
+benzene_pdb = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'benzene.pdb')
+benzene_bonds_pdb = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'benzene_bonds.pdb')
+benzene_atoms = ['C', 'H', 'C', 'H', 'C', 'H', 'C', 'H', 'C', 'H', 'C', 'H']
+benzene_coors = [[0.0000, 1.4027, 0.0000], [0.00000, 2.4903, 0.0000],
+                 [-1.2148, 0.7014, 0.0000], [-2.1567, 1.2451, 0.0000],
+                 [-1.2148, -0.7014, 0.0000], [-2.1567, -1.2451, 0.0000],
+                 [0.0000, -1.4027, 0.0000], [0.00000, -2.4903, 0.0000],
+                 [1.2148, -0.7014, 0.0000], [2.1567, -1.2451, 0.0000],
+                 [1.2148, 0.7014, 0.0000], [2.1567, 1.2451, 0.0000]]
+
+
+def test_write_pdb_benzene_molecule():
+    """Tests writing pdb formatted molecule file"""
+    benzene = Molecule(atoms=benzene_atoms, coordinates=benzene_coors)
+    test_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'benzene_test.pdb')
+    benzene.write(test_file, header='benzene')
+    assert filecmp.cmp(benzene_pdb, test_file)
+    os.remove(test_file)
+
+
+def test_write_pdb_benzene_molecule_with_bonds():
+    """Tests writing pdb formatted molecule file with bonds"""
+    benzene = Molecule(atoms=benzene_atoms, coordinates=benzene_coors)
+    test_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'benzene_bonds_test.pdb')
+    benzene.write(test_file, bonds=True, header='benzene')
+    assert filecmp.cmp(benzene_bonds_pdb, test_file)
+    os.remove(test_file)
