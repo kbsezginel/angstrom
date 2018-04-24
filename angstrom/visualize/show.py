52f6bffb5b13f548bdded463a56b6b35f7957480
kbsezginel <kbs37@pitt.edu>
2018-04-24 01:33:44 -0400

    Better docstring and argument refinement for show function.

--- angstrom/visualize/show.py
+++ angstrom/visualize/show.py
@@ -5,21 +5,26 @@ Works well with Jupyter notebook to visualize molecules in browser.
 """
 from .tools import arrange_molecules
 from angstrom.molecule.write import write_pdb
-import numpy as np
 import tempfile
 import nglview
 
 
-def show(*args, camera='perspective', move='auto', div=5, distance=(-10, -10), axis=0, caps=True, save=None, group=True):
-    """
-    Show given structures using nglview
+def show(*molecules, arrange=True, nx=5, distance=(-10, -10), camera='perspective', caps=True, save=None):
+    """ Show molecule objects using nglview.
+
+    Args:
+        - molecules: Any number Molecule objects.
+        - arrange (bool): Arrange structure positions (default: True).
+        - nx (int): Number of structures in x axis (horizontal).
+        - distance (tuple): Separation distance in x and y axes.
         - camera: 'perspective' / 'orthographic'
-        - move: separate multiple structures equally distant from each other
-        - distance: separation distance
-        - axis: separation direction
-        - caps: capitalize atom names so they show true colors in nglview
+        - caps (bool): Make atom names all capital letters (required for nglview to assign correct color).
+        - save (str / None): Save visualized structure in pdb format as given filename.
+
+    Returns:
+        - view: nglview "view" object
     """
-    atoms, coordinates, group_numbers = arrange_molecules(args, move=move, div=div, distance=distance, axis=axis, caps=caps, group=group)
+    atoms, coordinates, group_numbers = arrange_molecules(molecules, arrange=arrange, nx=nx, distance=distance, caps=caps)
 
     if save is None:
         temp_pdb_file = tempfile.NamedTemporaryFile(mode='w+', suffix='.pdb')
