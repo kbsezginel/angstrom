3189d2dfc8991df53604ef2fc159c2b57ce9fa22
kbsezginel <kbs37@pitt.edu>
2018-08-20 17:49:04 -0400

    Adds functional Blender image sequencer script.

--- angstrom/visualize/blender_sequencer.py
+++ angstrom/visualize/blender_sequencer.py
@@ -5,10 +5,57 @@ Squence a list of images and render a movie using Blender.
 import pickle
 import bpy
 import sys
+import os
+from PIL import Image
 
 
 def sequence_images(settings):
-    pass
+    """
+    Create a video of a list of images using Blender video sequencer.
+
+    Parameters
+    ----------
+    settings : dict
+        Blender sequencer settings.
+
+    Returns
+    -------
+    None
+        This function is called by the Blender Python installation.
+
+    """
+    # Create scene and sequence editor
+    scene = bpy.context.scene
+    scene.sequence_editor_create()
+
+    # Add background
+    background = scene.sequence_editor.sequences.new_effect(name='background', frame_start=1, frame_end=len(settings['images']) + 1, channel=1, type='COLOR')
+    background.color = settings['background_color']
+
+    # Add images
+    seq = scene.sequence_editor.sequences.new_image(name='img_strip', filepath=settings['images'][0], channel=2, frame_start=1)
+    seq.blend_type = 'ALPHA_OVER'
+    for f in settings['images'][1:]:
+        seq.elements.append(os.path.basename(f))
+
+    # Video settings
+    scene.render.fps = settings['fps']
+    scene.render.image_settings.file_format = settings['file_format']
+    scene.render.image_settings.quality = 100
+    scene.frame_end = len(settings['images'])
+
+    # Set video resolution from the first image
+    im = Image.open(settings['images'][0])
+    scene.render.resolution_x, scene.render.resolution_y = im.size
+    scene.render.resolution_percentage = 100
+
+    # Save .blend file
+    if settings['save'] != '':
+        bpy.ops.wm.save_as_mainfile(filepath=settings['save'])
+
+    if settings['render']:
+        scene.render.filepath = os.path.join(os.path.dirname(settings['images'][0]), 'angstrom')
+        bpy.ops.render.render(animation=True)
 
 
 if __name__ == '__main__':
